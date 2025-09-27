96a0ab217cc179f2999a145554f6dec0103e68e5
shamaz90 <shamaz90@gmail.com>
2025-09-27 15:23:54 +0300

    Add files via upload

--- addon.js
+++ addon.js
@@ -4,6 +4,21 @@ const express = require("express");
 
 const TMDB_API_KEY = "a6635913d6574e1d0acf79cacf6db07d";
 
+const GENRE_MAP = {
+    movie: {
+        "Action": 28, "Adventure": 12, "Animation": 16, "Comedy": 35, "Crime": 80,
+        "Documentary": 99, "Drama": 18, "Family": 10751, "Fantasy": 14, "History": 36,
+        "Horror": 27, "Music": 10402, "Mystery": 9648, "Romance": 10749, "Science Fiction": 878,
+        "TV Movie": 10770, "Thriller": 53, "War": 10752, "Western": 37
+    },
+    series: {
+        "Action & Adventure": 10759, "Animation": 16, "Comedy": 35, "Crime": 80,
+        "Documentary": 99, "Drama": 18, "Family": 10751, "Kids": 10762, "Mystery": 9648,
+        "News": 10763, "Reality": 10764, "Sci-Fi & Fantasy": 10765, "Soap": 10766,
+        "Talk": 10767, "War & Politics": 10768, "Western": 37
+    }
+};
+
 const manifest = {
     id: "org.korean.cinemeta",
     version: "1.0.0",
@@ -16,7 +31,7 @@ const manifest = {
             id: "korean-movies",
             name: "Korean Movies",
             extra: [
-                { name: "genre", isRequired: false },
+                { name: "genre", options: Object.keys(GENRE_MAP.movie), isRequired: false },
                 { name: "skip", isRequired: false }
             ]
         },
@@ -25,7 +40,7 @@ const manifest = {
             id: "korean-series",
             name: "Korean Series",
             extra: [
-                { name: "genre", isRequired: false },
+                { name: "genre", options: Object.keys(GENRE_MAP.series), isRequired: false },
                 { name: "skip", isRequired: false }
             ]
         }
@@ -36,17 +51,17 @@ const manifest = {
 
 const builder = new addonBuilder(manifest);
 
-async function fetchTMDBCatalog(type, genre, skip) {
-    const page = skip ? Math.floor(skip / 20) + 1 : 1;
-    let url = `https://api.themoviedb.org/3/discover/${type}?api_key=${TMDB_API_KEY}&with_original_language=ko&sort_by=popularity.desc&page=${page}`;
-    if (genre) {
-        url += `&with_genres=${genre}`;
-    }
+async function fetchTMDBCatalog(type, genreName, skip) {
+    const genreId = GENRE_MAP[type][genreName] || null;
+    const tmdbType = type === "movie" ? "movie" : "tv";
+    const page = Math.floor((skip || 0) / 20) + 1;
+    let url = `https://api.themoviedb.org/3/discover/${tmdbType}?api_key=${TMDB_API_KEY}&with_original_language=ko&sort_by=popularity.desc&page=${page}`;
+    if (genreId) url += `&with_genres=${genreId}`;
 
     const response = await axios.get(url);
     return response.data.results.map(item => ({
         id: "tmdb_" + item.id,
-        type: type === "movie" ? "movie" : "series",
+        type: type,
         name: item.title || item.name,
         poster: item.poster_path ? "https://image.tmdb.org/t/p/w500" + item.poster_path : null,
         background: item.backdrop_path ? "https://image.tmdb.org/t/p/w780" + item.backdrop_path : null,
@@ -57,8 +72,7 @@ async function fetchTMDBCatalog(type, genre, skip) {
 
 builder.defineCatalogHandler(async ({ type, id, extra }) => {
     if (id === "korean-movies" || id === "korean-series") {
-        const tmdbType = type === "movie" ? "movie" : "tv";
-        const metas = await fetchTMDBCatalog(tmdbType, extra.genre, extra.skip);
+        const metas = await fetchTMDBCatalog(type, extra.genre, parseInt(extra.skip || 0));
         return { metas };
     }
     return { metas: [] };
